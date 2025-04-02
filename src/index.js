239d7d680c3f85f4d1be6f51d42c6efe5653232c
Davinder Kumar <davinder.kumar.code@gmail.com>
2025-04-02 20:36:31 +0530

    added google oauth

--- src/index.js
+++ src/index.js
@@ -2,10 +2,34 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-
+const passport = require('passport');
+require('./config/passport-google');
+require('./config/passport-linkedin');
 app.use(express.json());
 app.use(cors());
 
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+// callback route for google => as user grants permission to our app 
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+    session: false,
+  }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 const port=process.env.PORT || 3000;
 
