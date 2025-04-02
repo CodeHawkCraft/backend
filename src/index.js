5a8c27a1472ac4b2110821d307197cea80ec0f0c
Davinder Kumar <davinder.kumar.code@gmail.com>
2025-04-02 16:56:27 +0530

    added changes

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+
+
+const port=process.env.PORT || 3000;
+
+app.listen(port, () => {
+
+})
+
